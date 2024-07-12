ed3140611d1bb3e861cf79577a1d84cee7f63ab8
ppvryocg8 <stararavind100@gmail.com>
2024-07-12 12:47:01 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDb = require('../backend/db/db.js');
+const mongoose=require('mongoose')
+
 const userRouter = require('./router/userRouter')
 const expenseRouter = require('./router/expenseRouter')
 const app = express();
@@ -10,7 +11,20 @@ app.use(cors());
 app.use(express.json());
 app.use('/auth',userRouter)
 app.use('/expenses',expenseRouter)
-connectDb();
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://thanush:thanush@expensetrack.nftzdvt.mongodb.net/?retryWrites=true&w=majority&appName=expensetrack')
+const db=mongoose.connection;
+
+db.on('open',()=>{
+    console.log('connected')
+})
+
+db.on('error',()=>{
+    console.log('not connected')
+})
+
+
 
 const port = 4000 || process.env.PORT_NO ;
 app.listen(port , ()=>{
